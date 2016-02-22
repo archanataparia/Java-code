5d30a8cb3dc1fbc8a7ad798a9d7a1452960b8ed0
archanataparia <archana_taparia@yahoo.co.in>
2016-02-21 22:52:14 -0800

    Update MergeSort.java

--- MergeSort.java
+++ MergeSort.java
@@ -1,5 +1,5 @@
 
-//only method need to submit in hackers rank two linked list sorted array is provided
+// two linked list sorted array is provided
 /*
   Merge two linked lists 
   head pointer input could be NULL as well for empty list
@@ -27,20 +27,7 @@
        }
 return headA;
 }
--------------------------------------------------------------------------------------- same code in python
-  def MergeLists(headA, headB):
-    # O(1) space complexity
 
-    if headA == None and headB == None: return None
-    if headA != None and headB == None: return headA
-    if headA == None and headB != None: return headB
-
-    if headA.data < headB.data:
-        headA.next = MergeLists(headA.next, headB)
-        return headA
-    else:
-        headB.next = MergeLists(headA, headB.next)
-        return headB
  ---------------------------------------------------------------------------------------------------
  second program-Find Merge Point of Two Lists
  ---------------------------------------------------------------------------------------------------
@@ -68,25 +55,3 @@ int FindMergeNode(Node headA, Node headB) {
 
 }
 
-
--------------------------------------------------------------------------------------- same code in python
-def FindMergeNode(headA, headB):
-    curA = headA
-    curB = headB
-    while not curA == curB:
-        if curA.next is None:
-            curA = headB
-        else:
-            curA = curA.next
-        if curB.next is None:
-            curB = headA
-        else:
-            curB = curB.next
-    return curA.data
- */
-
-/*----------------Merge list
- * from math import factorial as f
-for cas in xrange(input()):
-    a, b = map(int, raw_input().strip().split())
-    print f(a + b) / f(a) / f(b) % (10**9+7)*/
