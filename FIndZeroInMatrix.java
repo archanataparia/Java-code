acfed5b056e975dec40d927cf032a0735fe168bb
archanataparia <archana_taparia@yahoo.co.in>
2016-02-25 11:47:44 -0800

    Added files via upload

--- FIndZeroInMatrix.java
+++ FIndZeroInMatrix.java
@@ -5,30 +5,52 @@ package StringNArraysCTC;
 import java.util.Scanner;
 
 public class FIndZeroInMatrix {
-	static void findZeroInMatrix(int[][] a, int m, int n)
+	static void findZeroInMatrix(int[][] a, int r, int c)
 	{
-		for (int i=0;i<m;i++){
-			for (int j=0;j<n;j++)
+		boolean[] row = new boolean[r];
+		boolean[] column = new boolean[c];
+		
+		//store two arrays with flag if any value is 0 among row or column
+		for (int i=0;i<r;i++){
+			for (int j=0;j<c;j++)
 			{
 				if(a[i][j]==0)
 				{
-					
+					row[i]=true;
+					column[j]=true;
 				}
 			}
 		}
+		//set entire row and column 0
+		for (int i=0;i<r;i++){
+			for (int j=0;j<c;j++)
+			{
+				if(row[i] || column[j])
+				{
+					a[i][j]=0;
+				}
+			}
+		}
+		//print  matrix
+				for(int i=0;i<r;i++)
+				{
+					for(int j=0;j<c;j++)
+						System.out.print(a[i][j]+ " ");;
+					System.out.println();
+				}
 	}
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int m = sc.nextInt();
-		int n = sc.nextInt();
-		int[][] a = new int[m][n];
-		for(int i=0;i<m;i++)
+		int r = sc.nextInt();
+		int c = sc.nextInt();
+		int[][] a = new int[r][c];
+		for(int i=0;i<r;i++)
 		{
-			for(int j=0;j<n;j++)
+			for(int j=0;j<c;j++)
 				a[i][j]=sc.nextInt();
 		}
 		
-		findZeroInMatrix(a,m,n);
+		findZeroInMatrix(a,r,c);
 		sc.close();
 
 	}
